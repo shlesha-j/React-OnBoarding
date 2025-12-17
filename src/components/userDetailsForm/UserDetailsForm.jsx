3baddb6dfedb37305393023e335acd1c51cddfb9
Shlesha Joshi <joshishlesha33@gmail.com>
2025-12-17 19:30:48 +0530

    build bug fix

--- src/components/userDetailsForm/UserDetailsForm.jsx
+++ src/components/userDetailsForm/UserDetailsForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import "../userDetailsForm/userDetailsForm.css"
+import "../userDetailsForm/UserDetailsForm.css"
 import { replace, useNavigate } from 'react-router-dom';
 
 
